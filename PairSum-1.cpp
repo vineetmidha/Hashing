84f7d42aaeb24afc11db480f0755c66eb4236d6f
Vineet Midha <vineet.midha@yahoo.com>
2021-05-01 19:03:02 +0530

    GFG

---
+++ PairSum-1.cpp
@@ -0,0 +1,19 @@
+// https://practice.geeksforgeeks.org/problems/hashing-for-pair-1/0/?track=DSASP-Hashing&batchId=155
+
+int sumExists(int a[], int n, int sum)
+{
+   unordered_set<int> mp;
+   
+   for (int i=0; i<n; i++)
+   {
+        int target = sum - a[i];
+        
+        if (mp.count(target) > 0)
+            return 1;
+            
+        mp.insert(a[i]);
+   }
+   
+   return 0;
+}
+
