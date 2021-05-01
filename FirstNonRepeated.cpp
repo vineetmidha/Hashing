4aa851edf5422d5ed13b15a2c4c7c9dfd5300706
Vineet Midha <vineet.midha@yahoo.com>
2021-05-01 16:01:20 +0530

    GFG

--- FirstNonRepeated.cpp
+++ FirstNonRepeated.cpp
@@ -1,5 +1,13 @@
 // https://www.geeksforgeeks.org/non-repeating-element/
 
+/*
+TC: O(n)
+SC: (n)
+
+1. Traverse and create frequency map
+2. Traverse string again and check first character whose frequency is 1.
+*/
+    
 char firstNonRepeated(string &str)
 {
     if (str.size()==0) return '%';
