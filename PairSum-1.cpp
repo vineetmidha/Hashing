6cddfbb692299856f5f4e048734660d61f9b1a26
Vineet Midha <vineet.midha@yahoo.com>
2021-05-01 19:05:58 +0530

    GFG

--- PairSum-1.cpp
+++ PairSum-1.cpp
@@ -1,5 +1,24 @@
 // https://practice.geeksforgeeks.org/problems/hashing-for-pair-1/0/?track=DSASP-Hashing&batchId=155
 
+/*
+
+Naive: Nested loop
+1. Consider all possible pairs one by one and check for sum.
+2. If a pair is found, return true else return false
+
+Efficient-1: Sorted array with two-pointers
+TC : n log n
+Better if the array is already sorted
+
+Efficient-2:
+TC: O(n)
+
+1. Before inserting element in the hash table, we check if it forms a pair with any of the existing elements of the hash table
+2. If it forms a pair, return True.
+
+*/
+
+
 int sumExists(int a[], int n, int sum)
 {
    unordered_set<int> mp;
